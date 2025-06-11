877ad2ee7bef056fbef8925bba7e67e9e66fe5c8
MatexTheTurtle <80361557+MatexTheTurtle@users.noreply.github.com>
2025-06-11 04:28:17 -0700

    First and Final Commit

---
+++ TetrisLatest/Tetris/BlockQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+            //No duplicates
+            do
+            {
+                NextBlock = RandomBlock();
+            } while (block.ID == NextBlock.ID);
+
+            return block;
+        }
+    }
+}
---
+++ TetrisLatest/Tetris/GameState.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class GameState
+    {
+        private Block currentBlock;
+
+        public bool canHold { get; set; }
+        public Block heldBlock { get; set; }
+
+        public int score { get; set; }
+
+        //? I'm not sure how this works
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+
+        public GameGrid GameGrid { get; }
+        public BlockQueue BlockQueue { get; }
+        public bool GameOver { get; private set; }
+
+        public GameState()
+        {
+            GameGrid = new GameGrid(22, 10);
+            BlockQueue = new BlockQueue();
+            CurrentBlock = BlockQueue.GetAndUpdate();
+            canHold = true;
+        }
+
+        private bool BlockFits()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                if (!GameGrid.IsEmpty(p.Row, p.Column))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void HoldBlock()
+        {
+            if (!canHold) { return; }
+            if (heldBlock == null) { heldBlock = currentBlock; currentBlock = BlockQueue.GetAndUpdate(); }
+            else
+            {
+                Block temp = currentBlock;
+                currentBlock = heldBlock;
+                heldBlock = temp;
+            }
+            canHold = false;
+        }
+
+        public void RotateBlockCW()
+        {
+            CurrentBlock.RotateCW();
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateCCW();
+            }
+        }
+
+        public void RotateBlockCCW()
+        {
+            CurrentBlock.RotateCCW();
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateCW();
+            }
+        }
+
+        public void MoveBlockLeft()
+        {
+            CurrentBlock.Move(0, -1);
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, 1);
+            }
+        }
+
+        public void MoveBlockRight()
+        {
+            CurrentBlock.Move(0, 1);
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, -1);
+            }
+        }
+
+        private bool IsGameOver()
+        {
+            return !(GameGrid.IsRowEmpty(0) && GameGrid.IsRowEmpty(1));
+        }
+
+        private void PlaceBlock()
+        {
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                GameGrid[p.Row, p.Column] = CurrentBlock.ID;
+            }
+
+            score += GameGrid.ClearFullRows();
+            if (IsGameOver()) { GameOver = true; }
+            else { CurrentBlock = BlockQueue.GetAndUpdate(); canHold = true; }
+        }
+
+        public bool MoveBlockDown()
+        {
+            CurrentBlock.Move(1, 0);
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(-1, 0);
+                PlaceBlock();
+                return false;
+            }
+            return true;
+        }
+    }
+}
